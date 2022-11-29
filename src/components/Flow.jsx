a007588f35097f31fde45c22615025b517fddf92
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-11-29 19:00:21 +0100

    feat : update flow component

--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -2,23 +2,37 @@ import React from 'react';
 import { useState, useCallback } from 'react';
 import ReactFlow, { applyEdgeChanges, applyNodeChanges, Background, Controls, addEdge } from 'reactflow';
 import 'reactflow/dist/style.css';
-import startActivities from "../data/start_activities.json"
+import endActivities from "../data/start_activities.json"
 
-const initialEdges = []
+const endNode =
+    {
+        id: "Z",
+        position: { x: 1000, y: 800 },
+        data: { label: "End Node" },
+    }
 
-const initialNodes = Object.keys(startActivities).map((key, index) => {
+const initialEdges = Object.keys(endActivities).map((key, index) => {
+    return{
+        id: `${index + 1} - Z`, 
+        source: `${index + 1}`, 
+        target: `Z`, 
+        label: index,
+    }
+})
+
+const initialNodes = Object.keys(endActivities).map((key, index) => {
     return {
         id: `${index}`,
         position: {
             x: index * 10 * Math.floor(Math.random() * 5),
-            y: startActivities[key] * 10 * Math.floor(Math.random() * 5),
+            y: endActivities[key] * 10 * Math.floor(Math.random() * 5),
         },
         data: {label : key},
     };
 });
 
 const Flow = () => {
-    const [nodes, setNodes] = useState(initialNodes);
+    const [nodes, setNodes] = useState([...initialNodes, {...endNode}]);
     const [edges, setEdges] = useState(initialEdges);
 
     const onNodesChange = useCallback((changes) => setNodes((nds) => applyNodeChanges(changes, nds)), []);
